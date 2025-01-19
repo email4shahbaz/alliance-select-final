a59ca5d0d9532be81099df2940a9484d4be2309d
ShahbazAli <email4shahbaz@gmail.com>
2025-01-19 17:06:13 +0500

    all links on home menu and footer added on home page, contact form validation added

--- js/main.js
+++ js/main.js
@@ -2,17 +2,24 @@
 AOS.init();
 
 // Initialize Lozad
-const observer = lozad();
-observer.observe();
+//const observer = lozad();
+//observer.observe();
 
 const preloader=document.querySelector('#preloader');
 const pageOverlay = document.querySelector('#page-overlay');
 const navList = document.querySelector('.nav-list');
 
 document.addEventListener("DOMContentLoaded", function () {
-    // hamburgerBtn.addEventListener('click', () => {
-    //   navList.classList.toggle('drawer');
-    // });
+	const modalOverlay = document.createElement("div");
+	modalOverlay.className = "model-fade-bg";
+	document.body.appendChild(modalOverlay);
+
+	modalOverlay.addEventListener('click', () => {
+		document.body.classList.remove('mobile-menu-active');
+		document.querySelector('.nav-list').classList.remove('active')
+	});
+
+
 
 	document.querySelectorAll(".menu-item").forEach((item)=>{
 		item.addEventListener("click", function(){
@@ -23,18 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
 			this.classList.remove("active");
 		})
 	})
-
-
-
-	// if(drawerOverlay){
-	// 	drawerOverlay.addEventListener('click', ()=>{
-	// 		mainHeader.classList.remove("mobile");
-	// 		document.body.classList.remove("no-scroll");
-	// 		drawerOverlay.classList.remove("active");
-	// 	})
-	// }
-
-
 });
 
 
@@ -44,76 +39,26 @@ document.addEventListener("DOMContentLoaded", function () {
 const validateContactForm=()=>{
 	let isValid = true;
 
-	const name = document.querySelector('#fullname');
+	const name = document.querySelector('#name');
 	const email = document.querySelector('#email');
+	const contactNumber = document.querySelector('#contactNumber');
+	const subject = document.querySelector('#subject');
 	const message = document.querySelector('#message');
 
 	const nameError = document.querySelector('#nameError');
 	const emailError = document.querySelector('#emailError');
+	const contactError = document.querySelector('#contactError');
+	const subjectError = document.querySelector('#subjectError');
 	const messageError = document.querySelector('#messageError');
 
 	if (!name.value.trim()) {
 		nameError.style.display="block";
-		nameError.textContent = "Full name is required";
-		isValid = false;
-	} else {
-	nameError.textContent = "";
-	}
-
-	
-
-	if (!email.value.trim() || !email.validity.valid) {
-		emailError.style.display="block";
-		emailError.textContent = "A valid email id is required";
-		isValid = false;
-	} else {
-		emailError.textContent = "";
-	}
-
-	if(isValid==true){
-		pageOverlay.classList.add("active");
-		preloader.classList.add("active");
-		const timeout=setTimeout(()=>{
-			pageOverlay.classList.remove("active");
-			preloader.classList.remove("active");
-			document.querySelector('#contact-form').style.display='none';
-			document.querySelector('#contact-confirmation').style.display='grid';
-
-			document.querySelector('#btn-send-another').addEventListener('click', ()=>{
-				document.querySelector('#contact-form').style.display='grid';
-				document.querySelector('#contact-confirmation').style.display='none';
-			})
-
-			clearInterval(timeout);
-		}, 1000)
-	}
-
-
-	return false;
-}
-
-
-
-
-const validateRegisterForm=()=>{
-	let isValid = true;
-
-	const name = document.querySelector('#fullname');
-	const email = document.querySelector('#email');
-	const password = document.querySelector('#password');
-
-	const nameError = document.querySelector('#nameError');
-	const emailError = document.querySelector('#emailError');
-	const passwordError = document.querySelector('#passwordError');
-
-	if (!name.value.trim()) {
-		nameError.style.display="block";
-		nameError.textContent = "Full name is required";
+		nameError.textContent = "Please enter a name";
 		isValid = false;
 	} else {
 		nameError.textContent = "";
 	}
-
+	
 	if (!email.value.trim() || !email.validity.valid) {
 		emailError.style.display="block";
 		emailError.textContent = "A valid email id is required";
@@ -122,29 +67,34 @@ const validateRegisterForm=()=>{
 		emailError.textContent = "";
 	}
 
-	
-	if (!password.value.trim() && !password.value.length < 6) {
-		passwordError.style.display="block";
-		passwordError.textContent = "Please enter a password (not less than 6 characters)";
+	if (!contactNumber.value.trim()) {
+		contactError.style.display="block";
+		contactError.textContent = "Please enter a contact number";
 		isValid = false;
 	} else {
-		passwordError.textContent = "";
+		contactError.textContent = "";
 	}
 
-	if(isValid==true){
-		pageOverlay.classList.add("active");
-		preloader.classList.add("active");
-		const timeout=setTimeout(()=>{
-			pageOverlay.classList.remove("active");
-			preloader.classList.remove("active");
-			window.location="register-complete.html";
-			clearInterval(timeout);
-		}, 1000)
+	if (!subject.value.trim()) {
+		subjectError.style.display="block";
+		subjectError.textContent = "Please enter a subject";
+		isValid = false;
+	} else {
+		subjectError.textContent = "";
+	}
+
+	if (!message.value.trim()) {
+		messageError.style.display="block";
+		messageError.textContent = "Please enter a message";
+		isValid = false;
+	} else {
+		messageError.textContent = "";
 	}
 
 
-	return false;
+	return isValid;
 }
+
 
 
 // accordion start
@@ -162,8 +112,3 @@ document.querySelector('#hamburger-btn').addEventListener('click', () => {
 	document.querySelector('.nav-list').classList.toggle('active')
 });
 
-document.querySelector('.model-fade-bg').addEventListener('click', () => {
-	document.body.classList.remove('mobile-menu-active');
-	document.querySelector('.nav-list').classList.remove('active')
-});
-// mobile menu menu end
